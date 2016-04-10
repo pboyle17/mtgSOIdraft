665cd7c37649cbc6ed599f39d97596f0c9857870
Paul Boyle <pboyle17@gmail.com>
2016-04-09 19:41:56 -0500

    flipping single cards working now have to working on getting sets of cards working

--- app.js
+++ app.js
@@ -20,6 +20,60 @@ $.ajax({
       console.log(shadows.cards);
     };
 
+    shadows.makeURL = function(id){
+      var imgURL = 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=';
+      var imgURLend = '&type=card';
+      return imgURL+id+imgURLend;
+    }
+
+    shadows.makeComponent = function(id){
+      var url = shadows.makeURL(id);
+      return '<span class="card" id="'+id+'"><img src="'+url+'"></span>';
+    }
+
+    shadows.getCardObjFromMultiverseId = function(multiverseid){
+      var filtered = shadows.cards.filter(function(card){
+        if(card.multiverseid == multiverseid){
+          return card;
+        }
+      });
+
+      console.log(filtered[0]);
+      return filtered[0];
+    };
+
+    shadows.getCardObjFromCardName = function(cardName){
+      var filtered = shadows.cards.filter(function(card){
+        if(card.name == cardName){
+          return card;
+        }
+      });
+      console.log(filtered[0]);
+      return filtered[0];
+    };
+
+    shadows.changeCardImage = function(original,cardObj){
+      var URL = shadows.makeURL(cardObj.multiverseid);
+      console.log(URL);
+      var card = document.getElementById(original.multiverseid);
+      card.id=cardObj.multiverseid;
+      var img = card.firstChild;
+      img.src = URL;
+
+    };
+
+    shadows.flipCard = function(cardObj){
+      if(cardObj.name == cardObj.names[0]){
+        var flipped = shadows.getCardObjFromCardName(cardObj.names[1]);
+         shadows.changeCardImage(cardObj,flipped);
+      } else if (cardObj.name == cardObj.names[1]){
+        var flipped = shadows.getCardObjFromCardName(cardObj.names[0]);
+        shadows.changeCardImage(cardObj,flipped);
+      } else {
+        console.log('something went wrong!');
+      }
+    };
+
     shadows.commons = shadows.cards.filter(function(card){
         if(card.rarity == 'Common'){
           return card;
@@ -52,36 +106,27 @@ $.ajax({
       if(Array.isArray(arr)){
         for(var card in arr){
           id = arr[card].multiverseid;
-          imgHTMLstr = '<span class="card" id ="'+id+'"><img src="'+imgURL+id+imgURLend+'"></span>';
+          imgHTMLstr = shadows.makeComponent(id);
           $('.cards').append(imgHTMLstr);
           if(arr[card].layout == 'double-faced'){
+            console.log(arr[card]);
             $('#'+id).click(function(){
-              console.log(arr[card].names);
-              console.log('flip the card');
+              shadows.flipCard(arr[card]);
             });
           }
         }
       } else {
         id = arr.multiverseid;
-        imgHTMLstr = '<span class="card" id ="'+id+'"><img src="'+imgURL+id+imgURLend+'"></span>';
+        imgHTMLstr = shadows.makeComponent(id);
         $('.cards').append(imgHTMLstr);
         $('#'+id).click(function(){
-        var thing=shadows.cards.filter(function(card){
-          if(card.multiverseid == id){
-            return card;
+          var card = shadows.getCardObjFromMultiverseId(this.id);
+          if(card.layout == 'double-faced'){
+            shadows.flipCard(card);
           }
         });
-
-        if(thing.layout == 'double-faced'){
-          id++;
-          imgHTMLstr = '<span class="card" id ="'+id+'"><img src="'+imgURL+id+imgURLend+'"></span>';
-          $(this).attr('src',imgHTMLstr);
-        }
-      });//end of click listener
-
-
-    }//end of Array.isArray if/else block
-  };//end of render function
+      }
+    };
 
   },
   complete:function(){
